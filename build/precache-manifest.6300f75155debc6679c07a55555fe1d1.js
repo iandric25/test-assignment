5fdaa9cc124b30a83a6de4ecd790130d00185389
iandric25 <41676754+iandric25@users.noreply.github.com>
2020-04-16 15:09:05 +0200

    Add build

---
+++ build/precache-manifest.6300f75155debc6679c07a55555fe1d1.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a4ac98b90b23e13066290f207f918c29",
+    "url": "/index.html"
+  },
+  {
+    "revision": "3ca3a0c7c49481b2c7b1",
+    "url": "/static/css/2.16dd3be8.chunk.css"
+  },
+  {
+    "revision": "ab10fa88550c371d4a98",
+    "url": "/static/css/main.886615f7.chunk.css"
+  },
+  {
+    "revision": "3ca3a0c7c49481b2c7b1",
+    "url": "/static/js/2.7cf8b5ed.chunk.js"
+  },
+  {
+    "revision": "ed431f43256ddb11f74908756391fcb1",
+    "url": "/static/js/2.7cf8b5ed.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ab10fa88550c371d4a98",
+    "url": "/static/js/main.f1902c43.chunk.js"
+  },
+  {
+    "revision": "e755d7073a1c92b1f70a",
+    "url": "/static/js/runtime-main.26670210.js"
+  },
+  {
+    "revision": "8619a69461af172d6c353c575117de9f",
+    "url": "/static/media/defaultImg.8619a694.png"
+  }
+]);
